51409e6e04e81b830e4e765f94087d37a4959d09
10tekinvo@gmail.com <10tekinvo@gmail.com>
2020-07-23 15:36:20 +0600

    feat<Insta.DataAccess> Добавил контекст данных

---
+++ Insta/Insta.DataAccess/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Insta.Model;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Insta.DataAccess
+{
+    public class DataContext : IdentityDbContext<User>
+    {
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Like> Likes { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
